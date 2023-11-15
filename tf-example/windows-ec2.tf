7f381c52c64f8f9f458bd172c4324fdec60f7d5e
iahmad77 <150051463+iahmad77@users.noreply.github.com>
2023-11-15 08:43:39 -0500

    Create windows-ec2.tf

---
+++ tf-example/windows-ec2.tf
@@ -0,0 +1,37 @@
+# provision to us-east-2 region
+provider "aws" {
+  region  = "us-east-2"
+}
+
+#AWS Instance
+
+resource "aws_instance" "web_server" {
+     ami = data.aws_ami.windows.id
+     instance_type = "t2.micro"
+
+lifecycle {
+     ignore_changes = [ami]
+     }
+
+ }
+
+#AMI Filter for Windows Server 2019 Base
+
+data "aws_ami" "windows" {
+     most_recent = true
+
+     filter {
+        name   = "name"
+        values = ["Windows_Server-2019-English-Full-Base-*"]
+
+ }
+
+     filter {
+       name   = "virtualization-type"
+       values = ["hvm"]
+
+ }
+
+     owners = ["007666661890"] # Canonical
+
+ }
