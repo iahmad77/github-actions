b68c1395dbc8e671ec7cb6d2793c102f3e6a0fac
iahmad77 <150051463+iahmad77@users.noreply.github.com>
2023-11-14 20:16:29 -0500

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -1,19 +1,3 @@
-data "aws_ami" "ubuntu" {
-    most_recent = true
-
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
-    }
-
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-    
-    owners = ["007666661890"] # Canonical
-}
-
 provider "aws" {
   region  = "us-east-2"
 }
