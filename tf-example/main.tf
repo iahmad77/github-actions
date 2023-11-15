f6f635797d15fdb16106b6e80c85002ac4c937d9
iahmad77 <150051463+iahmad77@users.noreply.github.com>
2023-11-15 09:42:30 -0500

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -8,33 +8,18 @@ provider "aws" {
 resource "aws_instance" "web-server" {
      ami = data.aws_ami.windows.id
      instance_type = "t2.micro"
-
-lifecycle {
-     ignore_changes = [ami]
-     }
 tags = {
     Name = var.ec2_name
   }
 
  }
 
-#AMI Filter for Windows Server 2019 Base
-
-data "aws_ami" "windows" {
-     most_recent = true
-
-filter {
-        name   = "name"
-        values = ["Windows_Server-1909-English-Core-Base-*"]
-
- }
-
-filter {
-       name   = "virtualization-type"
-       values = ["hvm"]
-
- }
-
-owners = ["007666661890"] # Canonical
-
- }
+# Get latest Windows Server 2022 AMI
+data "aws_ami" "windows-2022" {
+  most_recent = true
+  owners      = ["007666661890"]
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2022-English-Full-Base*"]
+  }
+}
