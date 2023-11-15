9e5a518ac23d64911c3a2d72b8e9fb4e69821310
iahmad77 <150051463+iahmad77@users.noreply.github.com>
2023-11-14 20:33:52 -0500

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -1,13 +1,25 @@
-provider "aws" {
-  region  = "us-east-2"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-04e914639d0cca79a"
-  instance_type = "t2.micro"
-  key_name      = "app-ssh-key"
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
     Name = var.ec2_name
   }
 }
+
